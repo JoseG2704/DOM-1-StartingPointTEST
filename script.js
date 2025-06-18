1b7b481f1435828d87bf17a961d4ffa5e1739e8c
JoseG2704 <joseg50274@gmail.com>
2025-06-18 14:39:49 -0400

    12/12 completed and tested through the console

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Hello! If you see this, the script is working.");
 
+
 //#1 Select the section with an id of container without using querySelector.
 const NoQuery = document.getElementById("container");
 
@@ -14,28 +15,37 @@ const secondItems = document.getElementsByClassName("second");
 const ThirdInOl = document.querySelector("ol .third");
 
 //#5 Give the section with an id of container the text "Hello!".
-
+NoQuery.innerText = "Hello!";
 
 //#6 Add the class main to the div with a class of footer.
-
+const ClassFooter = document.querySelector(".footer");
+footer.classList.add("main");
 
 //#7 Remove the class main on the div with a class of footer.
-
+footer.classList.remove("main");
 
 //#8 Create a new li element.
-
+const LiElement = document.createElement("li");
 
 //#9 Give the li the text "four".
-
+LiElement.innerText = "four";
 
 //#10 Append the li to the ul element.
-
+const ul = document.querySelector("ul");
+ul.appendChild(LiElement);
 
 //#11 Loop over all of the lis inside the ol tag and give them a background color of "green".
-
+const olLis = document.querySelectorAll('ol li');
+olLis.forEach(li => {
+    li.style.backgroundColor = 'green';
+});
 
 //#12 Remove the div with a class of footer.
+const footerDiv = document.querySelector(".footer");
 
+if (footerDiv) {
+    footerDiv.remove();
+}
 
 // Try rewriting this without using querySelector
 const header = document.getElementById("container");
